b0a6ce646be08caa514f10dd5afa06fe9f7279cc
cavem <kyle.stinar@hotmail.com>
2022-02-10 11:08:55 -0800

    fixing back end

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
-app.use(express.static("views"));
-app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+const boardData = {};
 app.get("/", (req, res) => {
-  res.render("index", { title: "Trello Lite" });
+  res.render("index", boardData);
 });
 
-// app.post("/", (req, res) => {});
+app.post("/", (req, res) => {
+  console.log(req.body.name);
+  res.render("index", boardData);
+});
 
 app.listen(3000);
